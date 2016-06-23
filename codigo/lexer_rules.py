545d1d50e7b83d8860f05dbea6fe22a34ec2e827
Cyntia <gbonomi@dc.uba.ar>
2016-06-23 16:10:55 -0300

    Modificacion de la gramatica y arreglo de errores del lexer

--- codigo/lexer_rules.py
+++ codigo/lexer_rules.py
@@ -54,7 +54,9 @@ tokens = (
    'POW',
    'COLON',
    'VAR',
-   'COMMENT'
+   'COMMENT',
+   'STRING',
+   'COMA'
 )
 
 # Regular expression rules for simple tokens
@@ -65,12 +67,12 @@ t_DIVIDE  = r'/'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_MODULE = r'%'
-t_DOT = r'.'
+t_DOT = r'\.'
 t_LESS = r'<'
 t_GREATER = r'>'
-t_AND = r'AND'
-t_OR = r'OR'
-t_NOT = r'NOT'
+#t_AND = r'AND'
+#t_OR = r'OR'
+#t_NOT = r'NOT'
 t_PLUSEQUAL = r'\+='
 t_DIVEQUAL = r'/='
 t_MULEQUAL = r'\*='
@@ -81,35 +83,40 @@ t_SEMICOLON = r';'
 t_UNEQUAL = r'!='
 t_EQUAL = r'=='
 t_DECREMENT = r'--'
-t_INCREMENT = r'++'
-t_HASH = r'#'
+#t_INCREMENT = r'\++'
+t_HASH = r'\#'
 t_RKEY = r'\}'
 t_LKEY = r'\{'
-t_IF = r'if'
-t_ELSE = r'else'
-t_WHILE = r'while'
-t_FOR = r'for'
-t_DO = r'do'
-t_LENGTH = r'length'
-t_PRINT = r'print'
-t_MULTIPLICACIONESCALAR = r'multiplicacionEscalar'
-t_CAPITALIZAR = r'capitalizar'
-t_COLINEALES = r'colineales'
-t_QUESTIONMARK = r'?'
-t_TRUE = r'true'
-t_FALSE = r'false'
-t_BEGIN = r'begin'
-t_END = r'end'
-t_RETURN = r'return'
-t_RES = r'res'
+#t_IF = r'if'
+#t_ELSE = r'else'
+#t_WHILE = r'while'
+#t_FOR = r'for'
+#t_DO = r'do'
+#t_LENGTH = r'length'
+#t_PRINT = r'print'
+#t_MULTIPLICACIONESCALAR = r'multiplicacionEscalar'
+#t_CAPITALIZAR = r'capitalizar'
+#t_COLINEALES = r'colineales'
+t_QUESTIONMARK = r'\?'
+#t_TRUE = r'true'
+#t_FALSE = r'false'
+#t_BEGIN = r'begin'
+#t_END = r'end'
+#t_RETURN = r'return'
+#t_RES = r'res'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
 t_POW = r'\^'
 t_COLON = r':'
+t_COMA = r','
 
 
 #regular expression rules with some action code
 #Reserved symbols:
+
+def t_INCREMENT(t):
+    r'\+\+'
+    return t
 
 def t_ELSE(t):
 	r'else'
@@ -141,7 +148,7 @@ def t_DO(t):
 
 def t_RES(t):
 	r'res'
-	return t	
+	return t
 
 def t_RETURN(t):
 	r'return'
@@ -157,57 +164,62 @@ def t_FALSE(t):
 
 def t_AND(t):
 	r'and'
-	return t	
-	
+	return t
+
 def t_OR(t):
 	r'or'
-	return t	
-	
+	return t
+
 def t_NOT(t):
 	r'not'
-	return t	
-	
+	return t
+
 def t_PRINT(t):
 	r'print'
-	return t	
+	return t
 
 def t_MULTIPLICACIONESCALAR(t):
 	r'multiplicacionEscalar'
-	return t	
+	return t
 
 def t_CAPITALIZAR(t):
 	r'capitalizar'
-	return t	
+	return t
 
 def t_COLINEALES(t):
 	r'colineales'
-	return t	
+	return t
 
 def t_LENGTH(t):
 	r'length'
-	return t	
+	return t
 
 #end Reserved symbols
 
 def t_NATURAL(t):
     r'\d+'
-    t.value = int(t.value)    
+    t.value = int(t.value)
     return t
 
 def t_DECIMAL(t):
     r'\d+.\d+'
-    t.value = float(t.value)    
+    t.value = float(t.value)
     return t
 
 def t_VAR(t):
     r'[a-zA-Z]{1}[a-zA-Z0-9_]*'
-    t.value = string(t.value)    
+    t.value = t.value
+    return t
+
+def t_STRING(t):
+    r'".*"'
+    t.value = t.value
     return t
 
 def t_COMMENT(t):
-    r'#.'
-    t.value = string(t.value)    
-    return t    
+    r'\#(.|\n)*?\n'
+    t.value = t.value
+    return t
 
 # Define a rule so we can track line numbers
 def t_NEWLINE(t):
--- codigo/parser_rules.py
+++ codigo/parser_rules.py
@@ -3,13 +3,249 @@ from lexer_rules import tokens
 from expressions import Addition, Multiplication, Number
 
 
-def p_expression_plus(subexpressions):
-    'expression : expression PLUS term'
+def p_program(subexpressions):
+    'program : statements_list'
+    subexpressions[0] = subexpressions[1]
+
+def p_statements_list_statement(subexpressions):
+    'statements_list: statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_statements_list_append(subexpressions):
+    'statements_list: statements_list statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_e_semicolon(subexpressions):
+    'statement: e SEMICOLON'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_while(subexpressions):
+    'statement: while'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_if_else(subexpressions):
+    'statement: if_else'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_conditional(subexpressions):
+    'statement: conditional'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_for(subexpressions):
+    'statement: for'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_do_while(subexpressions):
+    'statement: do_while'
+    subexpressions[0] = subexpressions[1]
+
+def p_statement_comentario(subexpressions):
+    'statement: comment'
+    subexpressions[0] = subexpressions[1]
+
+def p_while_statement(subexpressions):
+    'while: WHILE LPAREN condition RPAREN statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_while_statements_list(subexpressions):
+    'while: WHILE LPAREN condition RPAREN RKEY statements_list LKEY'
+    subexpressions[0] = subexpressions[1]
+
+def p_if_else_statement(subexpressions):
+    'if_else: if ELSE statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_if_else_statements_list(subexpressions):
+    'if_else: if ELSE RKEY statements_list LKEY'
+    subexpressions[0] = subexpressions[1]
+
+def p_if_statement(subexpressions):
+    'if: IF LPAREN condition RPAREN statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_if_statements_list(subexpressions):
+    'if: IF LPAREN condition RPAREN RKEY statements_list LKEY'
+    subexpressions[0] = subexpressions[1]
+
+def p_conditional(subexpressions):
+    'conditional: LPAREN condition RPAREN QUESTIONMARK statement COLON statement'
+    subexpressions[0] = subexpressions[1]
+
+def p_for_statement(subexpressions):
+    'for: FOR LPAREN assignment SEMICOLON condition SEMICOLON advance RPAREN statemen'
+    subexpressions[0] = subexpressions[1]
+
+def p_for_statements_list(subexpressions):
+    'for: FOR LPAREN assignment SEMICOLON condition SEMICOLON advance RPAREN RKEY statements_list LKEY'
+    subexpressions[0] = subexpressions[1]
+
+def p_assignment_var(subexpressions):
+    'assignment: var ASSIGN expression'
+    subexpressions[0] = subexpressions[1]
+
+def p_assignment_vector(subexpressions):
+    'assignment: var LBRACKET INDEX RBRACKET ASSIGN expression'
+    subexpressions[0] = subexpressions[1]
+
+def p_assignment_record(subexpressions):
+    'assignment: var DOT CADENA ASSIGN expression'
+    subexpressions[0] = subexpressions[1]
+
+def p_assignment_empty(subexpressions):
+    'assignment:'
+    #subexpressions[0] = subexpressions[1]
+
+def p_advance_increment(subexpressions):
+    'advance: var INCREMENT'
+    subexpressions[0] = subexpressions[1]
+
+def p_advance_decrement(subexpressions):
+    'advance: var DECREMENT'
+    subexpressions[0] = subexpressions[1]
+
+def p_advance_minequal(subexpressions):
+    'advance: var MINEQUAL num'
+    subexpressions[0] = subexpressions[1]
+
+def p_advance_plusequal(subexpressions):
+    'advance: var PLUSEQUAL num'
+    subexpressions[0] = subexpressions[1]
+
+def p_advance_empty(subexpressions):
+    'advance:'
+    #subexpressions[0] = subexpressions[1]
+
+def p_do_while_statements_list(subexpressions):
+    'do_while: DO RKEY statements_list LKEY WHILE LPAREN condition RPAREN'
+    subexpressions[0] = subexpressions[1]
+
+def p_condition_logical_condition(subexpressions):
+    'condition:logical_condition'
+    subexpressions[0] = subexpressions[1]
+
+def p_condition_and_condition(subexpressions):
+    'condition: and_condition'
+    subexpressions[0] = subexpressions[1]
+
+def p_condition_or_condition(subexpressions):
+    'condition: or_condition'
+    subexpressions[0] = subexpressions[1]
+
+def p_logical_condition_less(subexpressions):
+    'logical_condition: e1 LESS e2'
+    subexpressions[0] = subexpressions[1]
+
+def p_logical_condition_greater(subexpressions):
+    'logical_condition: e1 GREATER e2'
+    subexpressions[0] = subexpressions[1]
+
+def p_logical_condition_equal(subexpressions):
+    'logical_condition: e1 EQUAL e2'
+    subexpressions[0] = subexpressions[1]
+
+def p_logical_condition_unequal(subexpressions):
+    'logical_condition: e1 UNEQUAL e2'
+    subexpressions[0] = subexpressions[1]
+
+def p_and_condition(subexpressions):
+    'and_condition: logical_condition AND logical_condition'
+    subexpressions[0] = subexpressions[1]
+
+def p_or_condition(subexpressions):
+    'or_condition: logical_condition OR logical_condition'
+    subexpressions[0] = subexpressions[1]
+
+def p_e_assignment(subexpressions):
+    'e: assignment'
+    subexpressions[0] = subexpressions[1]
+
+def p_e_expression(subexpressions):
+    'e: expression'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_string(subexpressions):
+    'value: STRING'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_bool(subexpressions):
+    'value: BOOL'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_var(subexpressions):
+    'value: VAR'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_num(subexpressions):
+    'value: NUM'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_function(subexpressions):
+    'value: function'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_vector(subexpressions):
+    'value: var LBRACKET indice RBRACKET'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_registro(subexpressions):
+    'value: var DOT cadena'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_list_vector(subexpressions):
+    'value: LBRACKET list_values RBRACKET'
+    subexpressions[0] = subexpressions[1]
+
+def p_value_list_records(subexpressions):
+    'value: RKEY list_records LKEY'
+    subexpressions[0] = subexpressions[1]
+
+def p_list_records_record(subexpressions):
+    'list_records: record'
+    subexpressions[0] = subexpressions[1]
+
+def p_list_records_record(subexpressions):
+    'list_records: record'
+    subexpressions[0] = subexpressions[1]
+
+def p_list_records_append(subexpressions):
+    'list_records: list_records COMA record'
+    subexpressions[0] = subexpressions[1]
+
+def p_recor(subexpressions):
+    'record: CADENA DOT value'
+    subexpressions[0] = subexpressions[1]
+
+def p_list_values_value(subexpressions):
+    'list_values: value'
+    subexpressions[0] = subexpressions[1]
+
+def p_list_values_append(subexpressions):
+    'list_values: list_values COMA value'
+    subexpressions[0] = subexpressions[1]
+
+
+def p_expression_arithmetic(subexpressions):
+    'expression: expression_arithmetic'
+    subexpressions[0] = subexpressions[1]
+
+def p_expression_value(subexpressions):
+    'expression: value'
+    subexpressions[0] = subexpressions[1]
+
+
+def p_arithmetic_expression_plus(subexpressions):
+    'arithmetic_expression : arithmetic_expression PLUS termino'
     subexpressions[0] = Addition(subexpressions[1], subexpressions[3])
 
 
-def p_expression_term(subexpressions):
-    'expression : term'
+def p_arithmetic_expression_minus(subexpressions):
+    'arithmetic_expression : arithmetic_expression MINUS term'
+    subexpressions[0] = Subtract(subexpressions[1], subexpressions[3])
+
+
+
+def p_arithmetic_expression_term(subexpressions):
+    'arithmetic_expression : term'
     subexpressions[0] = subexpressions[1]
 
 
@@ -18,19 +254,36 @@ def p_term_times(subexpressions):
     subexpressions[0] = Multiplication(subexpressions[1], subexpressions[3])
 
 
+def p_term_divide(subexpressions):
+    'term : term DIVIDE factor'
+    subexpressions[0] = Division(subexpressions[1], subexpressions[3])
+
+def p_term_module(subexpressions):
+    'term : term MODULE factor'
+    subexpressions[0] = Module(subexpressions[1], subexpressions[3])
+
 def p_term_factor(subexpressions):
     'term : factor'
     subexpressions[0] = subexpressions[1]
 
-
+#OJO CON ESTO PORQUE NOSOTROS TENEMOS DIVIDIDO ENTRE NATURALES Y DECIMALES
 def p_factor_number(subexpressions):
     'factor : NUMBER'
     subexpressions[0] = Number(subexpressions[1])
 
+#ACA CUAL ES EL VALOR DE var[INDICE]? de donde sale?
+def p_factor_vector(subexpressions):
+    'factor : var[INDICE]'
+    subexpressions[0] = Number(subexpressions[1])
 
-def p_factor_expression(subexpressions):
-    'factor : LPAREN expression RPAREN'
-    subexpressions[0] = subexpressions[2]
+#ACA CUAL ES EL VALOR DE var.cadena? de donde sale?
+def p_factor_registro(subexpressions):
+    'factor : var.cadena'
+    subexpressions[0] = Number(subexpressions[1])
+
+#def p_factor_expression(subexpressions):
+#    'factor : LPAREN expression RPAREN
+#    subexpressions[0] = subexpressions[2]
 
 
 def p_error(subexpressions):
