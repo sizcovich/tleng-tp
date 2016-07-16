8746c681e46fe146679fd7bd37e3a3ff0f00edc3
Sabrina Izcovich <sizcovich@dc.uba.ar>
2016-07-16 13:46:28 -0300

    SLS parser para correrlo como piden en el enunciado

---
+++ codigo/SLSparser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Similar to C++ parser made in Python"""
+from __future__ import print_function
+import argparse
+import sys
+import lexer_rules
+import parser_rules
+from sys import argv, exit
+import lex
+import yacc
+
+def parseInput() :
+    parser = argparse.ArgumentParser(
+        description='A C++ basic parser',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,# ArgumentDefaultsHelpFormatter
+        epilog='''Authors: Cyntia Bonomi, Alan Castro and Sabrina Izcovich''')
+    parser.add_argument('-o', type=argparse.FileType('w'), nargs='?',  default=sys.stdout, help='outputFilename')
+    parser.add_argument('-c', type=argparse.FileType('r'), nargs='?', default=sys.stdin, help='inputFilename')
+    args = parser.parse_args()
+    return parser,args;
+
+def dump_ast(ast, output_file):
+    output_file.write("digraph {\n")
+    key = frozenset(ast.items())
+    edges = []
+    queue = [ast]
+    numbers = {ast: 1}
+    current_number = 2
+    while len(queue) > 0:
+        node = queue.pop(0)
+        name = node.name()
+        number = numbers[node]
+        output_file.write('node[width=1.5, height=1.5, shape="circle", label="%s"] n%d;\n' % (name, number))
+        for child in node.children():
+            numbers[child] = current_number
+            edge = 'n%d -> n%d;\n' % (number, current_number)
+            edges.append(edge)
+            queue.append(child)
+            current_number += 1
+    output_file.write("".join(edges))
+    output_file.write("}")
+
+
+if __name__ == "__main__":
+    parser,args = parseInput()
+    if parser.parse_args(['-o', '-c']):
+        text = (args.c).read()
+        lexer = lex.lex(module=lexer_rules)
+        parser = yacc.yacc(module=parser_rules)
+        ast = parser.parse(text, lexer)
+        print(ast["value"],file = args.o)
+    elif parser.parse_args([]):
+        lexer = lex.lex(module=lexer_rules)
+        parser = yacc.yacc(module=parser_rules)
+        ast = parser.parse(infile, lexer)
+        print(ast["value"])
+    elif parser.parse_args(['-c']):
+        text = (args.c).read()
+        input_file.close()
+        lexer = lex.lex(module=lexer_rules)
+        parser = yacc.yacc(module=parser_rules)
+        ast = parser.parse(text, lexer)
+        print(ast["value"])
+    elif parser.parse_args(['-o']):
+        lexer = lex.lex(module=lexer_rules)
+        parser = yacc.yacc(module=parser_rules)
+        ast = parser.parse(infile, lexer)
+        print(ast["value"],file = args.o)
+    else:
+        print("The program should be called as ./SLSParser [-o EXIT] [-c ENTRY | SOURCE]")
